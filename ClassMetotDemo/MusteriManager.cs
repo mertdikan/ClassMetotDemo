2b8317cf023f3f920ef390afaad8fddf7ab00a15
mertdikan <mert-61-ts@hotmail.com>
2021-01-19 02:17:52 +0300

    Kamp Odev
    Kamp odev2

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteriler)
+        {
+            Console.WriteLine("{0} {1}, sistemimize eklendi", musteriler.Ad, musteriler.Soyad);
+
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            Console.WriteLine("\tAdı\t\tSoyadı\t\tEposta\t\t\t\tŞehir");
+            for (int i = 0; i < musteriler.Length; i++)
+            {
+                Console.WriteLine("\t{0}\t\t{1}\t\t{2}\t\t{3}", musteriler[i].Ad, musteriler[i].Soyad, musteriler[i].TcNo, musteriler[i].Sehir);
+            }
+
+        }
+
+        public void Sil(Musteri musteriler)
+        {
+            Console.WriteLine("{0} {1}, adlı müşteri kaydı silindi!", musteriler.Ad, musteriler.Soyad);
+            
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Mert";
+            musteri1.Soyad = "DİKAN";
+            musteri1.TcNo = 1234566197;
+            musteri1.Sehir = "Istanbul";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Uğurcan";
+            musteri2.Soyad = "ÇAKIR";
+            musteri2.TcNo = 1234566196;
+            musteri2.Sehir = "Trabzon";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Vitor";
+            musteri3.Soyad = "HUGO";
+            musteri3.TcNo = 1234566190;
+            musteri3.Sehir = "Trabzon";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+
+
+            Console.WriteLine("\n---------------------------MÜŞTERİ LİSTESİ-----------------------------");
+            musteriManager.Listele(musteriler);
+
+
+            Console.WriteLine("\n -----------------------------------------------------------------------");
+            musteriManager.Sil(musteri2);
+            Console.ReadLine();
+
+
+
+
+        }
+         
+    }
+}
